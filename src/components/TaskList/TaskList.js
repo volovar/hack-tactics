470ea41e141a8320c51b2d0a2db7ff1db6d1a364
volovar <michaelvolovar@gmail.com>
2019-06-18 23:50:18 -0400

    [TCTCS | 062019] Remove Temp Task Data, Update Styling
    Removed the temp data used to populate tasks early in development.
    
    Added additional styling to make things look a bit nicer and to test out
    the column layout on smaller screens.

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -2,6 +2,7 @@ import React from "react";
 import Task from "components/Task/Task";
 import { useTaskState } from "contexts/TaskContext";
 import { useDayState } from "contexts/DayContext";
+import { css } from "emotion";
 
 const TaskList = () => {
     const { tasks } = useTaskState();
@@ -9,7 +10,16 @@ const TaskList = () => {
     const filteredTasks = tasks.filter(task => task.day === currentDay);
 
     return filteredTasks ? (
-        <ul>
+        <ul
+            className={css`
+                list-style: none;
+                padding: 0;
+                display: flex;
+                flex-direction: column;
+                flex-grow: 1;
+                overflow: auto;
+            `}
+        >
             {filteredTasks.map((task, i) => (
                 <Task key={`task-${i}`} task={task} />
             ))}
